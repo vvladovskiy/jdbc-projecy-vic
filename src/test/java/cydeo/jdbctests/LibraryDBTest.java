a8398a6a40e23a4d3944c0bd3798298767134c9c
Vick Wladowski <vvladovskiy@gmail.com>
2022-07-27 15:53:54 -1000

    My SQL project

--- src/test/java/cydeo/jdbctests/LibraryDBTest.java
+++ src/test/java/cydeo/jdbctests/LibraryDBTest.java
@@ -1,8 +1,11 @@
 package cydeo.jdbctests;
 
+import cydeo.jdbctests.utility.DB_Util;
+
 public class LibraryDBTest {
     //@Test
     public void testUserCount(){
+        DB_Util.createConnection();
 
 
     }
