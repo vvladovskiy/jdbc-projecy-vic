73d74d54417c0bb782751f00052110ede7956a46
Vick Wladowski <vvladovskiy@gmail.com>
2022-07-28 13:56:38 -1000

    My SQL project

--- src/test/java/cydeo/jdbctests/LibraryDBTest.java
+++ src/test/java/cydeo/jdbctests/LibraryDBTest.java
@@ -1,9 +1,10 @@
 package cydeo.jdbctests;
 
 import cydeo.jdbctests.utility.DB_Util;
+import org.junit.jupiter.api.Test;
 
 public class LibraryDBTest {
-    //@Test
+    @Test
     public void testUserCount(){
         DB_Util.createConnection();
 
